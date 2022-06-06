53813d73817d3b90c1f7f37bb4c76f64ca36746b
Jenni <nvy0128@gmail.com>
2022-06-06 14:34:13 +0700

    Enter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import { Landing } from './pages';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p className='fw-bold'>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
+            <Landing />
 		</div>
 	);
 }
